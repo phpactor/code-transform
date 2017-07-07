892227e7751e41da61eb199276be77d85a08c5a3
dantleech <dan.t.leech@gmail.com>
2017-07-07 20:51:41 +0100

    TODO: Fix reflection type class to return decent string values

---
+++ tests/Adapter/WorseReflection/ImplementContractsTest.php
@@ -0,0 +1,206 @@
+<?php
+
+namespace Phpactor\CodeTransform\Tests\Adapter\WorseReflection;
+
+use Phpactor\CodeTransform\Domain\SourceCode;
+
+use PHPUnit\Framework\TestCase;
+use Phpactor\CodeTransform\Adapter\WorseReflection\ImplementContracts;
+use Phpactor\WorseReflection\SourceCodeLocator\StringSourceLocator;
+use Phpactor\WorseReflection\Reflector;
+use Phpactor\WorseReflection\SourceCode as WorseSourceCode;
+
+class ImplementContractsTest extends TestCase
+{
+    /**
+     * @dataProvider provideCompleteConstructor
+     */
+    public function testImplementContracts(string $example, string $expected)
+    {
+        $source = SourceCode::fromString($example);
+        $transformer = new ImplementContracts(new Reflector(new StringSourceLocator(WorseSourceCode::fromString((string) $source))));
+        $source = $transformer->transform($source);
+        $this->assertEquals((string) $expected, (string) $source);
+    }
+
+    public function provideCompleteConstructor()
+    {
+        return [
+            'It does nothing on source with no classes' => [
+                <<<'EOT'
+<?php
+
+EOT
+                , 
+                <<<'EOT'
+<?php
+
+EOT
+
+            ],
+            'It does nothing on class with no interfaces or parent classes' => [
+                <<<'EOT'
+<?php
+
+class Foobar
+{
+}
+EOT
+                , 
+                <<<'EOT'
+<?php
+
+class Foobar
+{
+}
+EOT
+
+            ],
+            'It implements an interface' => [
+                <<<'EOT'
+<?php
+
+interface Rabbit
+{
+    public function dig(int $depth = 5): Dirt
+}
+
+class Foobar implements Rabbit
+{
+}
+EOT
+                , 
+                <<<'EOT'
+<?php
+
+interface Rabbit
+{
+    public function dig(int $depth = 5): Dirt
+}
+
+class Foobar implements Rabbit
+{
+    public function dig(int $depth = 5): Dirt
+    {
+    }
+}
+EOT
+
+            ],
+            'It implements multiple interfaces' => [
+                <<<'EOT'
+<?php
+
+interface Dog
+{
+    public function bark(int $volume = 11): Sound
+}
+
+interface Rabbit
+{
+    public function dig(int $depth = 5): Dirt
+}
+
+class Foobar implements Rabbit, Dog
+{
+}
+EOT
+                , 
+                <<<'EOT'
+<?php
+
+interface Dog
+{
+    public function bark(int $volume = 11): Sound
+}
+
+interface Rabbit
+{
+    public function dig(int $depth = 5): Dirt
+}
+
+class Foobar implements Rabbit, Dog
+{
+    public function dig(int $depth = 5): Dirt
+    {
+    }
+
+    public function bark(int $volume = 11): Sound
+    {
+    }
+}
+EOT
+
+            ],
+            'It does adds inherit docblocks' => [
+                <<<'EOT'
+<?php
+
+interface Bird
+{
+    /**
+     * Emit chirping sound.
+     */
+    public functio chirp();
+}
+
+class Foobar implements Bird
+{
+}
+EOT
+                , 
+                <<<'EOT'
+<?php
+
+interface Bird
+{
+    /**
+     * Emit chirping sound.
+     */
+    public function chirp();
+}
+
+class Foobar implements Bird
+{
+    /**
+     * Emit chirping sound.
+     */
+    public function chirp()
+    {
+    }
+}
+EOT
+
+            ],
+            'It is idempotent' => [
+                <<<'EOT'
+<?php
+
+interface Bird
+{
+    public function chirp();
+}
+
+class Foobar implements Bird
+{
+    public function chirp() {}
+}
+EOT
+                , 
+                <<<'EOT'
+<?php
+
+interface Bird
+{
+    public function chirp();
+}
+
+class Foobar implements Bird
+{
+    public function chirp() {}
+}
+EOT
+            ],
+        ];
+    }
+}
