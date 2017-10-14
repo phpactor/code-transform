7e47065c6298123889800dfa659422b1bcccd708
dantleech <dan.t.leech@gmail.com>
2017-10-14 20:46:05 +0200

    Generate method works

--- lib/Adapter/WorseReflection/Refactor/WorseGenerateMethod.php
+++ lib/Adapter/WorseReflection/Refactor/WorseGenerateMethod.php
@@ -16,6 +16,9 @@ use Phpactor\WorseReflection\Core\Reflection\Inference\Symbol;
 use Phpactor\WorseReflection\Core\Reflection\Inference\SymbolInformation;
 use Phpactor\WorseReflection\Core\SourceCode;
 use Phpactor\WorseReflection\Reflector;
+use Phpactor\WorseReflection\Bridge\TolerantParser\Reflection\Collection\ReflectionArgumentCollection;
+use Phpactor\WorseReflection\Bridge\TolerantParser\Reflection\AbstractReflectionMethodCall;
+use Phpactor\WorseReflection\Core\Reflection\ReflectionArgument;
 
 class WorseGenerateMethod implements GenerateMethod
 {
@@ -31,127 +34,53 @@ class WorseGenerateMethod implements GenerateMethod
      */
     private $updater;
 
-    /**
-     * @var Parser
-     */
-    private $parser;
-
     /** @var int
      */
     private $methodSuffixIndex = 0;
 
-    public function __construct(Reflector $reflector, Updater $updater, Parser $parser = null)
+    public function __construct(Reflector $reflector, Updater $updater)
     {
         $this->reflector = $reflector;
         $this->updater = $updater;
-        $this->parser = $parser ?: new Parser();
     }
 
     public function generateMethod(string $sourceCode, int $offset, $methodName = null)
     {
-        $parameters = $this->parameters($sourceCode, $offset);
-        $sourceCode = $this->addMethod($sourceCode, $offset, $parameters);
+        $methodCall = $this->reflector->reflectMethodCall($sourceCode, $offset);
+        $prototype = $this->generatePrototype($methodCall, $methodName);
 
-        return $sourceCode;
+        return $this->updater->apply($prototype, Code::fromString($sourceCode));
     }
 
-    private function parameters(string $sourceCode, int $offset)
+    private function generatePrototype(AbstractReflectionMethodCall $methodCall, string $methodName = null)
     {
-        $node = $this->parser->parseSourceFile($sourceCode);
-        $methodNode = $node->getDescendantNodeAtPosition($offset);
-
-        if ($methodNode instanceof MemberAccessExpression) {
-            $methodNode = $methodNode->getParent();
-        }
-
-        if (!isset($methodNode->parent->expression->callableExpression->dereferencableExpression)) {
-            throw new \InvalidArgumentException(sprintf(
-                'Only $this->methodName() for generation is supported'
-            ));
-        }
-
-        $base = $methodNode->parent->expression->callableExpression->dereferencableExpression->name->getText($sourceCode);
-
-        if ($base !== self::VAR_NAME_THIS) {
-            throw new \InvalidArgumentException(
-                'Only support generating new methods on ' . self::VAR_NAME_THIS
-            );
-        }
-
-        if (false === $methodNode instanceof CallExpression) {
-            throw new \InvalidArgumentException(sprintf(
-                'Node does not part of a call expression, it is a "%s"',
-                get_class($methodNode)
-            ));
-        }
-
-        if (null === $methodNode->argumentExpressionList) {
-            return [];
-        }
-
-        $parameters = [];
-
-        /** @var ArgumentExpression $expression */
-        foreach ($methodNode->argumentExpressionList->children as $expression) {
-            // NOTE: This is _really_ inefficient, the code is parsed, and the
-            //       frames resolved for EACH parameter.
-            $offset = $this->reflector->reflectOffset(
-                SourceCode::fromString($sourceCode),
-                Offset::fromInt($expression->getEndPosition())
-            );
-
-            $parameters[] = $offset->symbolInformation();
-        }
-
-        return $parameters;
-    }
-
-    private function addMethod(string $sourceCode, int $offset, array $parameters, string $methodName = null)
-    {
-        $offset = $this->reflector
-            ->reflectOffset(SourceCode::fromString($sourceCode), Offset::fromInt($offset));
-        $symbolInformation = $offset->symbolInformation();
-
-        $symbol = $symbolInformation->symbol();
-        $frame = $offset->frame();
-
-        $methodName = $methodName ?: $symbol->name();
-
-        $thisClassVar = $frame->locals()->byName(self::VAR_NAME_THIS)->first();
-        $thisClass = $this->reflector->reflectClassLike($thisClassVar->symbolInformation()->type()->className());
+        $methodName = $methodName ?: $methodCall->name();
 
         $builder = SourceCodeBuilder::create();
-        $builder->namespace((string) $thisClass->name()->namespace());
-        $classBuilder = $builder->class((string) $thisClass->name()->short());
+        $builder->namespace((string) $methodCall->class()->name()->namespace());
+
+        $classBuilder = $builder->class((string) $methodCall->class()->name()->short());
         $methodBuilder = $classBuilder->method($methodName);
         $methodBuilder->visibility(Visibility::PRIVATE);
 
-        /** @var SymbolInformation $parameter */
-        foreach ($parameters as $parameter) {
-            $type = $parameter->type();
+        /** @var ReflectionArgument $argument */
+        foreach ($methodCall->arguments() as $argument) {
+            $type = $argument->type();
 
-            $parameterName = $parameter->symbol()->name();
-
-            if ($parameterName === Symbol::UNKNOWN) {
-                $parameterName = sprintf('param%s', $this->methodSuffixIndex++);
-            }
-
-            $parameterBuilder = $methodBuilder->parameter($parameterName);
+            $argumentBuilder = $methodBuilder->parameter($argument->guessName());
 
             if ($type->isDefined()) {
                 if ($type->isPrimitive()) {
-                    $parameterBuilder->type((string) $type);
+                    $argumentBuilder->type((string) $type);
                 }
 
                 if ($type->isClass()) {
-                    $parameterBuilder->type($type->className()->short());
+                    $argumentBuilder->type($type->className()->short());
                 }
             }
         }
 
-        $prototype = $builder->build();
-
-        return $this->updater->apply($prototype, Code::fromString($sourceCode));
+        return $builder->build();
     }
 }
 
