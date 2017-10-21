2443e5eb046de03ed2a3b23202a95379746520d8
dantleech <dan.t.leech@gmail.com>
2017-10-21 10:39:15 +0200

    rename variable

--- lib/Adapter/TolerantParser/Refactor/TolerantRenameVariable.php
+++ lib/Adapter/TolerantParser/Refactor/TolerantRenameVariable.php
@@ -8,8 +8,11 @@ use Microsoft\PhpParser\Node\Expression\Variable;
 use Microsoft\PhpParser\Node\SourceFileNode;
 use Microsoft\PhpParser\TextEdit;
 use Microsoft\PhpParser\Node;
+use Phpactor\CodeTransform\Domain\Refactor\RenameVariable;
+use Microsoft\PhpParser\FunctionLike;
+use Microsoft\PhpParser\ClassLike;
 
-class TolerantRenameVariable
+class TolerantRenameVariable implements RenameVariable
 {
     /**
      * @var Parser
@@ -21,11 +24,12 @@ class TolerantRenameVariable
         $this->parser = $parser;
     }
 
-    public function renameVariable(string $source, int $offset, string $newName): SourceCode
+    public function renameVariable(string $source, int $offset, string $newName, string $scope = self::SCOPE_FILE): SourceCode
     {
         $sourceNode = $this->sourceNode($source);
         $variable = $this->variableFromSource($sourceNode, $offset);
-        $textEdits = $this->textEditsToRename($sourceNode, $variable, $newName);
+        $scopeNode = $this->scopeNode($variable, $scope);
+        $textEdits = $this->textEditsToRename($scopeNode, $variable, $newName);
 
         return SourceCode::fromString(TextEdit::applyEdits($textEdits, $source));
     }
@@ -40,10 +44,10 @@ class TolerantRenameVariable
         return $sourceNode->getDescendantNodeAtPosition($offset);
     }
 
-    private function textEditsToRename(SourceFileNode $sourceNode, Variable $variable, string $newName): array
+    private function textEditsToRename(Node $scopeNode, Variable $variable, string $newName): array
     {
         /** @var Node $node */
-        foreach ($sourceNode->getDescendantNodes() as $node) {
+        foreach ($scopeNode->getDescendantNodes() as $node) {
             if (false === $node instanceof Variable) {
                 continue;
             }
@@ -57,5 +61,22 @@ class TolerantRenameVariable
 
         return $textEdits;
     }
-}
 
+    private function scopeNode(Variable $variable, string $scope): Node
+    {
+        if ($scope === RenameVariable::SCOPE_FILE) {
+            return $variable->getRoot();
+        }
+
+        $scopeNode = $variable->getFirstAncestor(FunctionLike::class, ClassLike::class, SourceFileNode::class);
+
+        if (null === $scopeNode) {
+            throw new \RuntimeException(
+                'Could not determine scope node, this should not happen as ' .
+                'there should always be a SourceFileNode.'
+            );
+        }
+
+        return $scopeNode;
+    }
+}
--- tests/Adapter/TolerantParser/Refactor/TolerantRenameVariableTest.php
+++ tests/Adapter/TolerantParser/Refactor/TolerantRenameVariableTest.php
@@ -4,18 +4,19 @@ namespace Phpactor\CodeTransform\Tests\Adapter\TolerantParser\Refactor;
 
 use Phpactor\CodeTransform\Tests\Adapter\TolerantParser\TolerantTestCase;
 use Phpactor\CodeTransform\Adapter\TolerantParser\Refactor\TolerantRenameVariable;
+use Phpactor\CodeTransform\Domain\Refactor\RenameVariable;
 
 class TolerantRenameVariableTest extends TolerantTestCase
 {
     /**
      * @dataProvider provideRenameMethod
      */
-    public function testRenameVariable(string $test, int $offset, $name)
+    public function testRenameVariable(string $test, int $offset, $name, string $scope = RenameVariable::SCOPE_FILE)
     {
         list($source, $expected) = $this->splitInitialAndExpectedSource(__DIR__ . '/fixtures/' . $test);
 
-        $extractConstant = new TolerantRenameVariable($this->parser());
-        $transformed = $extractConstant->renameVariable($source, $offset, $name);
+        $renameVariable = new TolerantRenameVariable($this->parser());
+        $transformed = $renameVariable->renameVariable($source, $offset, $name, $scope);
 
         $this->assertEquals(trim($expected), trim($transformed));
     }
@@ -23,11 +24,22 @@ class TolerantRenameVariableTest extends TolerantTestCase
     public function provideRenameMethod()
     {
         return [
-            [
+            'one instance no context' => [
                 'renameVariable1.test',
                 9,
                 'newName'
-            ]
+            ],
+            'two instances no context' => [
+                'renameVariable2.test',
+                9,
+                'newName'
+            ],
+            'local scope' => [
+                'renameVariable3.test',
+                83,
+                'newName',
+                RenameVariable::SCOPE_LOCAL
+            ],
         ];
     }
 }
