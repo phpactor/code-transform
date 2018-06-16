e9f29a48cd68c69a9ee5d688ce0e1a69fcaceafa
dantleech <dan.t.leech@gmail.com>
2018-06-16 17:47:47 +0200

    Extract expression

---
+++ lib/Adapter/TolerantParser/Refactor/TolerantExtractExpression.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace Phpactor\CodeTransform\Adapter\TolerantParser\Refactor;
+
+use Exception;
+use Microsoft\PhpParser\Node;
+use Microsoft\PhpParser\Node\StatementNode;
+use Microsoft\PhpParser\Node\Statement\ExpressionStatement;
+use Microsoft\PhpParser\Parser;
+use Microsoft\PhpParser\TextEdit;
+use Phpactor\CodeTransform\Domain\Exception\TransformException;
+use Phpactor\CodeTransform\Domain\SourceCode;
+
+class TolerantExtractExpression
+{
+    /**
+     * @var Parser
+     */
+    private $parser;
+
+    public function __construct(Parser $parser = null)
+    {
+        $this->parser = $parser ?: new Parser();
+    }
+
+    public function extractExpression(SourceCode $source, int $offsetStart, int $offsetEnd = null, string $variableName): SourceCode
+    {
+        $rootNode = $this->parser->parseSourceFile((string) $source);
+        $startNode = $rootNode->getDescendantNodeAtPosition($offsetStart);
+
+        if ($offsetEnd) {
+            $endNode = $rootNode->getDescendantNodeAtPosition($offsetEnd);
+        }
+
+        if (null === $startNode) {
+            return $source;
+        }
+
+        $startPosition = $startNode->getStart();
+
+        $endPosition = $offsetEnd ? $endNode->getEndPosition() : $startNode->getEndPosition();
+        $extractedString = rtrim(trim($source->extractSelection($startPosition, $endPosition)), ';');
+        $assigment = sprintf('$%s = %s;', $variableName, $extractedString) . PHP_EOL;
+
+        $statement = $startNode->getFirstAncestor(StatementNode::class);
+
+        if (null === $statement) {
+            return $source;
+        }
+
+        $edits = $this->resolveEdits($statement, $startNode, $extractedString, $assigment, $variableName);
+
+        return $source->withSource(TextEdit::applyEdits($edits, (string) $source));
+    }
+
+    private function resolveEdits(
+        Node $statement,
+        Node $startNode,
+        string $extractedString,
+        string $assigment,
+        string $variableName
+    )
+    {
+        // replace entire statement
+        if ($statement->getStart() === $startNode->getStart()) {
+            return [
+                new TextEdit($statement->getStart(), $statement->getWidth(), $assigment)
+            ];
+        }
+        
+        return [
+            new TextEdit($statement->getStart(), 0, $assigment),
+            new TextEdit($startNode->getStart(), strlen($extractedString), '$' . $variableName),
+        ];
+    }
+}
---
+++ tests/Adapter/TolerantParser/Refactor/TolerantExtractExpressionTest.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace Phpactor\CodeTransform\Tests\Adapter\TolerantParser\Refactor;
+
+use Exception;
+use Phpactor\CodeTransform\Adapter\TolerantParser\Refactor\TolerantExtractExpression;
+use Phpactor\CodeTransform\Domain\SourceCode;
+use Phpactor\CodeTransform\Tests\Adapter\TolerantParser\TolerantTestCase;
+
+class TolerantExtractExpressionTest extends TolerantTestCase
+{
+    /**
+     * @dataProvider provideExtractExpression
+     */
+    public function testExtractExpression(string $test, string $name, string $expectedExceptionMessage = null)
+    {
+        list($source, $expected, $offsetStart, $offsetEnd) = $this->sourceExpectedAndOffset(__DIR__ . '/fixtures/' . $test);
+
+        if ($expectedExceptionMessage) {
+            $this->expectException(Exception::class);
+            $this->expectExceptionMessage($expectedExceptionMessage);
+        }
+
+        $extractMethod = new TolerantExtractExpression();
+        $transformed = $extractMethod->extractExpression(SourceCode::fromString($source), $offsetStart, $offsetEnd, $name);
+
+        $this->assertEquals(trim($expected), trim($transformed));
+    }
+
+    public function provideExtractExpression()
+    {
+        yield 'no op' => [
+            'extractExpression1.test',
+            'foobar',
+        ];
+
+        yield 'extract string literal' => [
+            'extractExpression2.test',
+            'foobar',
+        ];
+
+        yield 'extract on end position semi-colon' => [
+            'extractExpression3.test',
+            'foobar',
+        ];
+
+        yield 'single node' => [
+            'extractExpression4.test',
+            'foobar',
+        ];
+
+        yield 'single node' => [
+            'extractExpression4.test',
+            'foobar',
+        ];
+
+        yield 'single array expression' => [
+            'extractExpression5.test',
+            'foobar',
+        ];
+
+        yield 'single stand-alone array expression' => [
+            'extractExpression6.test',
+            'foobar',
+        ];
+    }
+}
